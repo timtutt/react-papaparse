07f7279d60a3948eceacd119a08d396b0b7a5a93
Bunlong <bunlong.van@gmail.com>
2020-04-04 10:46:13 +0700

    :rocket:

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,16 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from '@rollup/plugin-node-resolve'
-import url from '@rollup/plugin-url'
-import svgr from '@svgr/rollup'
-import pkg from './package.json'
-// https://github.com/rollup/rollup-plugin-node-resolve#resolving-built-ins-like-fs
-import builtins from 'builtin-modules'
+import typescript from '@rollup/plugin-typescript';
+import babel from 'rollup-plugin-babel';
+import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
-  ],
-  external: builtins,
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+  },
   plugins: [
-    external(),
-    postcss({
-      modules: true,
-      extensions: ['.css']
-    }),
-    url({ exclude: ['**/*.svg'] }),
-    svgr(),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: []
-    }),
-    resolve({ preferBuiltins: true }),
-    commonjs()
-  ]
-}
+    typescript(),
+    babel(),
+    terser(),
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { readString } from './readString'
